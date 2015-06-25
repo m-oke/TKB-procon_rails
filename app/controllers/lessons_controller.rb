62d70a538ef6ee25e3d29f5bd3d337884c0afb8d
m-oke <s1420616@u.tsukuba.ac.jp>
2015-06-25 12:30:31 +0900

    Fix question show view to show answer data.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -12,7 +12,7 @@ class LessonsController < ApplicationController
 
   private
   def set_lesson
-    @lesson = Lesson.find(params[:lesson_id])
+    @lesson = Lesson.find(params[:id])
   end
 
   def get_students
